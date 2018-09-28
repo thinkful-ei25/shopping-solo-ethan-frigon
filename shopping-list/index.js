515b9974529c165b210994aee17296e83e8c98f5
talexcrowell <talexcrowell@gmail.com>
2018-09-28 09:06:16 -0700

    modified generateItemElement and generateShoppingItemsString

--- shopping-list/index.js
+++ shopping-list/index.js
@@ -7,13 +7,39 @@ const STORE = [
   {name: 'bread', checked: false}
 ];
 
+function generateItemElement(item, itemIndex, template ) {
+  return `
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+}
+
+function generateShoppingItemsString(shoppingList) {
+  console.log('Generating shopping list element');
+  
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+
+  return items.join('');
+}
+
 
 function renderShoppinglist() {
   //render the shopping list in the DOM
-  const shoppingListItemsString = '<li>apples</li>';
+  
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
 
   $('.js-shopping-list').html(shoppingListItemsString);
   console.log('`renderShoppinglist` ran');
+
+  
 }
 
 function handleNewItemSubmit() {
@@ -38,3 +64,5 @@ function handleShoppingList() {
   handleDeleteItemClicked();
 
 }
+
+$(handleShoppingList);
